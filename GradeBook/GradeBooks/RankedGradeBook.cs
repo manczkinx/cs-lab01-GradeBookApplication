ad1171255cfc0a9ff39b9a4e52ed78c65cf1b66a
manczkinx <sepioool24@gmail.com>
2023-04-08 02:27:17 +0200

    Utowrzenie klas RankedGradeBook i StandardGradeBook

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+
+
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+            Name = name;
+        }
+
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            return base.GetLetterGrade(averageGrade) {
+                if (Students.Count > 5)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                int i = 0;
+                double[] LetterTop = new double[Students.Count];
+                foreach (var student in Students)
+                {
+                    LetterTop[i] = student.AverageGrade;
+                    i++;
+                }
+
+                Array.Sort;
+                Console.WriteLine(LetterTop.Length);
+                double LetterPlace = 0;
+                double c = LetterTop.Length;
+                double d = 0;
+
+                for (int j = 0; j < LetterTop.Length; j++)
+                {
+                    d = (j - c) * -1;
+                    if (LetterTop[j] == averageGrade)
+                    {
+                        LetterPlace = (d / c) * 100;
+                    }
+                    double LetterPlace 2 = ((j + 1) / c) * 100;
+                    Console.WriteLine(LetterTop[j] + " " + j + " " + LetterPlace + " " + LetterPlace2 + " " + averageGrade);
+                    Console.WriteLine(LetterTop[j] == averageGrade;
+                }
+
+                if (LetterPlace <= 20)
+                {
+                    return 'A';
+                }
+
+                else if (LetterPlace > 20 && LetterPlace <= 40)
+                {
+                    return 'B';
+                }
+
+                else if (LetterPlace > 40 && LetterPlace <= 60)
+                {
+                    return 'C';
+                }
+
+                else if (LetterPlace > 60 && LetterPlace <= 80)
+                {
+                    return 'D';
+                }
+
+                else
+                {
+                    return 'F';
+                }
+
+
+            }
+
+        public ovveride void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+
+            if (Students.Count >= 5)
+                base.CalculateStatistics();
+        }
+    
+
+                }
+            }  
+        }
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+
+            Type = Enums.GradeBookType.Standard;
+            Name = name;
+        }    
+    }
+}
